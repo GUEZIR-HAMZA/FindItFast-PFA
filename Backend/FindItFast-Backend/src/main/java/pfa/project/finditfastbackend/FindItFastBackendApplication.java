87b07407eee196bee63d08210d9eb78185a8a443
GUEZIR-HAMZA <95761219+GUEZIR-HAMZA@users.noreply.github.com>
2023-03-29 18:17:03 +0000

    commit test

--- Backend/FindItFast-Backend/src/main/java/pfa/project/finditfastbackend/FindItFastBackendApplication.java
+++ Backend/FindItFast-Backend/src/main/java/pfa/project/finditfastbackend/FindItFastBackendApplication.java
@@ -8,6 +8,7 @@ public class FindItFastBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FindItFastBackendApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
