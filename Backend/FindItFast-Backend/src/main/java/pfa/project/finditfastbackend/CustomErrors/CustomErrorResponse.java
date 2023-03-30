08cfedb80b95aeb01306e80658ae0d108f4c5802
GUEZIR-HAMZA <95761219+GUEZIR-HAMZA@users.noreply.github.com>
2023-03-30 20:03:23 +0000

    - final version of login and register backend - tested with swagger OpenAPI and workbench database

--- Backend/FindItFast-Backend/src/main/java/pfa/project/finditfastbackend/CustomErrors/CustomErrorResponse.java
+++ Backend/FindItFast-Backend/src/main/java/pfa/project/finditfastbackend/CustomErrors/CustomErrorResponse.java
@@ -1,4 +1,4 @@
-package pfa.project.finditfastbackend.Controller;
+package pfa.project.finditfastbackend.CustomErrors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
