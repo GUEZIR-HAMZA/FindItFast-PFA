6d2fa4fa5b5786fd78deb9c16d27d9a8c496156e
Ines-ElM <127637751+Ines-ElM@users.noreply.github.com>
2023-04-03 23:57:41 +0000

    Create App.js

---
+++ Frontend/FindItFast-Pages/src/components/auth/Login-register/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import * as Components from './Components';
+
+function App() {
+  const [signIn, toggle] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = async (event) => {
+    event.preventDefault();
+    const response = await fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    });
+    const data = await response.json();
+    console.log(data); // Handle response from server
+  };
+
+  const handleSignIn = async (event) => {
+    event.preventDefault();
+    const response = await fetch("/api/authenticate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    });
+    const data = await response.json();
+    console.log(data); // Handle response from server
+  };
+      return(
+          <Components.Container>
+              <Components.SignUpContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Create Account</Components.Title>
+                      <Components.Input type='text' placeholder='Name' />
+                      <Components.Input type='email' placeholder='Email' />
+                      <Components.Input type='password' placeholder='Password' />
+                      <Components.Button1>Sign Up</Components.Button1>
+                  </Components.Form>
+              </Components.SignUpContainer>
+
+              <Components.SignInContainer signinIn={signIn}>
+                   <Components.Form>
+                       <Components.Title>Sign in</Components.Title>
+                       <Components.Input type='email' placeholder='Email' />
+                       <Components.Input type='password' placeholder='Password' />
+                       <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                       <Components.Button2>Sign In</Components.Button2>
+                   </Components.Form>
+              </Components.SignInContainer>
+
+              <Components.OverlayContainer signinIn={signIn}>
+                  <Components.Overlay signinIn={signIn}>
+
+                  <Components.LeftOverlayPanel signinIn={signIn}>
+                      <Components.Title>Welcome Back!</Components.Title>
+                      <Components.Paragraph>
+                          To keep connected with us please login with your personal info
+                      </Components.Paragraph>
+                      <Components.GhostButton onClick={() => toggle(true)}>
+                          Sign In
+                      </Components.GhostButton>
+                      </Components.LeftOverlayPanel>
+
+                      <Components.RightOverlayPanel signinIn={signIn}>
+                        <Components.Title>Hello, Friend!</Components.Title>
+                        <Components.Paragraph>
+                            Enter Your personal details and start journey with us
+                        </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(false)}>
+                                Sign Up
+                            </Components.GhostButton> 
+                      </Components.RightOverlayPanel>
+  
+                  </Components.Overlay>
+              </Components.OverlayContainer>
+
+          </Components.Container>
+      )
+ }
+
+ export default App;
