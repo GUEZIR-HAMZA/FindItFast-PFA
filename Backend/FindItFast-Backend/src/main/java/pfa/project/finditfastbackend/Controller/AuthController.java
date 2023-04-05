8538f639bfc49b7e527a6be4640000ef15d43846
Nour <noureddine.maroub@emsi-edu.ma>
2023-04-05 01:21:23 +0000

    Adding DigitalCard Services with Implementations.

--- Backend/FindItFast-Backend/src/main/java/pfa/project/finditfastbackend/Controller/AuthController.java
+++ Backend/FindItFast-Backend/src/main/java/pfa/project/finditfastbackend/Controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pfa.project.finditfastbackend.Model.User;
-import pfa.project.finditfastbackend.Service.UserService;
+import pfa.project.finditfastbackend.Service.user.UserService;
 import pfa.project.finditfastbackend.CustomExceptions.UserExceptions.AuthenticationException;
 import pfa.project.finditfastbackend.CustomExceptions.UserExceptions.UserAlreadyExistException;
 
---
+++ Backend/FindItFast-Backend/src/main/java/pfa/project/finditfastbackend/Service/digitalcard/DigitalCardServiceImpl.java
@@ -0,0 +1,32 @@
+package pfa.project.finditfastbackend.Service.digitalcard;
+
+import pfa.project.finditfastbackend.Model.DigitalCard;
+
+import java.util.List;
+
+public class DigitalCardServiceImpl implements DigitalCardService{
+    @Override
+    public boolean addDigitalCard(DigitalCard digitalCard) {
+        return false;
+    }
+
+    @Override
+    public boolean updateDigitalCard(DigitalCard digitalCard) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteDigitalCard(DigitalCard digitalCard) {
+        return false;
+    }
+
+    @Override
+    public List<DigitalCard> getAllDigitalCards() {
+        return null;
+    }
+
+    @Override
+    public DigitalCard getDigitalCardById(Long id) {
+        return null;
+    }
+}
